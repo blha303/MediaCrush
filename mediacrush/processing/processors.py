e8040a2137181f388648836aafb51e3e207eefb1
Drew DeVault <sir@cmpwn.com>
2013-12-13 22:27:49 -0800

    Generate CSS for extracted subtitle fonts

--- mediacrush/processing/processors.py
+++ mediacrush/processing/processors.py
@@ -1,4 +1,7 @@
 from mediacrush.processing.processor import Processor
+from mediacrush.processing.invocation import Invocation
+from mediacrush.config import _cfg
+import os
 
 copy = "cp {0} {1}.{extension}"
 
@@ -26,12 +29,14 @@ class VideoProcessor(Processor):
             map_string += ' -map 0:a:0'
         self._execute("ffmpeg -y -i {0} -q 5 -pix_fmt yuv420p -acodec libvorbis -vcodec libtheora" + map_string + " {1}.ogv")
         # Extract extra streams if present
+        fonts = []
         if self.extra['has_fonts'] or stream.extra['has_subtitles']:
             for stream in self.extra['streams']:
                 if stream['type'] == 'font':
                     # Note that ffmpeg returns a nonzero exit code when dumping attachments because there's technically no output file
                     # -dump_attachment is a mechanism completely removed from the rest of the ffmpeg workflow
                     self._execute("ffmpeg -y -dump_attachment:t:0:" + str(stream["index"]) + ' {1}_attachment_' + stream["extra"] + ' -i {0}', ignoreNonZero=True)
+                    fonts.append(stream)
                 elif stream['type'] == 'subtitle':
                     extension = None
                     if stream['extra']['codec_name'] == 'ssa':
@@ -42,6 +47,32 @@ class VideoProcessor(Processor):
                         extension = '.vtt'
                     if extension != None:
                         self._execute("ffmpeg -y -i {0} -map 0:s:0 {1}" + extension)
+        # Examine font files and construct some CSS to import them
+        css = ''
+        for font in fonts:
+            command = Invocation('otfinfo --info {0}')
+            command(os.path.join(_cfg("storage_folder"), '%s_attachment_%s' % (self.f.hash, font["extra"])))
+            command.run()
+            output = command.stdout[0].split('\n')
+            family = None
+            subfamily = None
+            for line in output:
+                if line.startswith('Family:'):
+                    family = line[7:].strip(' \t')
+                if line.startswith('Subfamily:'):
+                    subfamily = line[10:].strip(' \t')
+            css += '@font-face{font-family: "%s";' % family
+            css += 'src:url("/%s_attachment_%s");' % (self.f.hash, font["extra"])
+            if subfamily == 'SemiBold':
+                css += 'font-weight: 600;'
+            elif subfamily == 'Bold':
+                css += 'font-weight: bold;'
+            elif subfamily == 'Italic':
+                css += 'font-style: italic;'
+            css += '}'
+        css_file = open(os.path.join(_cfg("storage_folder"), '%s_fonts.css' % self.f.hash), 'w')
+        css_file.write(css)
+        css_file.close()
 
 class AudioProcessor(Processor):
     time = 300
